e945e7181c650f7cc3624412bd3dbbb1f6207505
Joel D <imaginethere.snoie@gmail.com>
2021-11-13 20:07:32 +0200

    Made data files with info about rovers

---
+++ src/data/allRovers.js
@@ -0,0 +1,1 @@
+export const allRovers = ['Curiosity', 'Opportunity', 'Spirit'];
---
+++ src/data/roversCamerasInfo.js
@@ -0,0 +1,47 @@
+export const roverCamerasInfo = [
+  {
+    abbr: 'FHAZ',
+    fullName: 'Front Hazard Avoidance Camera',
+    rovers: ['Curiosity', 'Opportunity', 'Spirit']
+  },
+  {
+    abbr: 'RHAZ',
+    fullName: 'Rear Hazard Avoidance Camera',
+    rovers: ['Curiosity', 'Opportunity', 'Spirit']
+  },
+  {
+    abbr: 'MAST',
+    fullName: 'Mast Camera',
+    rovers: ['Curiosity']
+  },
+  {
+    abbr: 'CHEMCAM',
+    fullName: 'Chemistry and Camera Complex',
+    rovers: ['Curiosity']
+  },
+  {
+    abbr: 'MAHLI',
+    fullName: 'Mars Hand Lens Imager',
+    rovers: ['Curiosity']
+  },
+  {
+    abbr: 'MARDI',
+    fullName: 'Mars Descent Imager',
+    rovers: ['Curiosity']
+  },
+  {
+    abbr: 'NAVCAM',
+    fullName: 'Navigation Camera',
+    rovers: ['Curiosity', 'Opportunity', 'Spirit']
+  },
+  {
+    abbr: 'PANCAM',
+    fullName: 'Panoramic Camera',
+    rovers: ['Opportunity', 'Spirit']
+  },
+  {
+    abbr: 'MINITES',
+    fullName: 'Miniature Thermal Emission Spectrometer (Mini-TES)\t',
+    rovers: ['Opportunity', 'Spirit']
+  },
+];
