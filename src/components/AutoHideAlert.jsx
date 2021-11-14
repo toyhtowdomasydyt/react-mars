2d39ab18eeb483ba57172bad5d5303c2f5fb7b35
Joel D <imaginethere.snoie@gmail.com>
2021-11-14 23:38:41 +0200

    Made auto hide Alert component

---
+++ src/components/AutoHideAlert.jsx
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {Alert, AlertTitle, Snackbar, Typography} from "@mui/material";
+
+class AutoHideAlert extends Component {
+  constructor({type, title, body, delayMs}) {
+    super({type, title, body, delayMs});
+
+    this.state = {
+      open: true
+    }
+  }
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState(() => ({open: false}));
+  };
+
+
+  render() {
+    const {type, title, body, delayMs} = this.props;
+
+    return (
+      <Snackbar
+        open={this.state.open}
+        autoHideDuration={delayMs}
+        onClose={this.handleClose}
+        sx={{position: 'static'}}
+      >
+        <Alert severity={type} variant="filled" sx={{width: '100%'}} onClose={this.handleClose}>
+          <AlertTitle>
+            {title}
+          </AlertTitle>
+          <Typography variant="body2" color="text.secondary">
+            {body}
+          </Typography>
+        </Alert>
+      </Snackbar>
+    );
+  }
+}
+
+export default AutoHideAlert;
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 
-import {Grid, Card, CardContent, Typography} from '@mui/material';
+import {Grid} from '@mui/material';
 
 import ImageCard from './ImageCard';
+import AutoHideAlert from './AutoHideAlert';
 
 const Result = ({resultData, searched}) => {
   return (
-    <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+    <Grid container rowSpacing={3} columnSpacing={{xs: 1, sm: 2, md: 3}}>
       {
         resultData.length === 0 && searched && (
           <Grid item xs={12}>
-            <Card>
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Sorry, there are not photos for current search query
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Please, try another one queries combination
-                </Typography>
-              </CardContent>
-            </Card>
+            <AutoHideAlert
+              type='warning'
+              delayMs={6000}
+              title='Sorry, there are not photos for current search query'
+              body='Please, try another one queries combination'
+            />
           </Grid>
         )
       }
