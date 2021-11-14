362f1950ca06b4a6c47b723e0eed2b79458cc520
Joel D <imaginethere.snoie@gmail.com>
2021-11-14 16:26:15 +0200

    Add Select component

---
+++ src/components/Select.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {InputLabel, MenuItem, Select} from '@mui/material';
+
+const BasicSelect = ({name, label, id, value, options, handleChange}) => {
+  return (
+    <>
+      <InputLabel id={`label-${id}`}>{label}</InputLabel>
+      <Select
+        id={id}
+        name={name}
+        labelId={`label-${id}`}
+        value={value}
+        label={label}
+        onChange={handleChange}
+      >
+        {options.map(item => (
+          <MenuItem key={item.abbr} value={item.abbr}>{item.fullName}</MenuItem>
+        ))}
+      </Select>
+    </>
+  );
+}
+
+export default BasicSelect;
